d05108a6685b7e62d55855d9098dcae3840d9603
Shubham Ghelani <76262127+ShubhamG2311@users.noreply.github.com>
2023-01-06 20:14:16 +0530

    Add files via upload

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': '3874469fe9mshc27af044dfff1f2p142925jsna34f8ea3d65c',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
